463376f8b6fe421b385083724c77cf514c0b6e95
Liam <ljemmeson@gmail.com>
2023-12-08 16:17:09 +0900

    Updated config for future projects

--- launch/start.py
+++ launch/start.py
@@ -4,9 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
+    # rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
 
     return LaunchDescription([
         Node(namespace='hesai_ros_driver', package='hesai_ros_driver', executable='hesai_ros_driver_node', output='screen'),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
+        # Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
     ])
