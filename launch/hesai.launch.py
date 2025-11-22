8a37772ffb693aadd103b6059467ef500352c4c4
leonardonels <goldenmatrix2.0@gmail.com>
2025-11-22 11:11:54 +0100

    changes made to the launch file and config file to use standrd ros2 paramters parsing

--- launch/hesai.launch.py
+++ launch/hesai.launch.py
@@ -1,9 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
+    config_dir = os.path.join(
+        get_package_share_directory('hesai_ros_driver'), 
+        'config',
+        'ros_config.yaml'
+        )
+    
     return LaunchDescription([
-        Node(namespace='hesai_ros_driver', package='hesai_ros_driver', executable='hesai_ros_driver_node', output='screen'),
+        Node(
+            name='hesai_driver_node',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            output='screen',
+            parameters=[config_dir]),
     ])
